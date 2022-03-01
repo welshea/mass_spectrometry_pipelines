b450b90d54e49e7462a46cc565882c0254289c0c
Eric A. Welsh <eric.welsh@moffitt.org>
2022-03-01 14:06:58 -0500

    support LipidSearch mergeResult.txt;
    MOD:  split_lipidomics_into_pos_neg.pl;
          [Tue Mar  1 14:06:58 2022 -0500]

--- split_lipidomics_into_pos_neg.pl
+++ split_lipidomics_into_pos_neg.pl
@@ -1,8 +1,35 @@
 #!/usr/bin/perl -w
 
+# 2022-03-01  infer unknown adduct charge from known adducts
+# 2022-02-28  major changes to support LipidSearch mergeResult.txt
+# 2021-11-30  initial release
+
 
 use Scalar::Util qw(looks_like_number);
 use POSIX;
+
+
+# known ion charges
+$known_ion_charge_hash{'+H'}     = 'pos';
+$known_ion_charge_hash{'+NH4'}   = 'pos';
+$known_ion_charge_hash{'+Na'}    = 'pos';
+$known_ion_charge_hash{'+H-H2O'} = 'pos';
+$known_ion_charge_hash{'-H'}     = 'neg';
+$known_ion_charge_hash{'+HCOO'}  = 'neg';
+
+$keep_header_hash{'Rej.'} = 1;
+$keep_header_hash{'LipidIon'} = 1;
+$keep_header_hash{'LipidGroup'} = 1;
+$keep_header_hash{'Class'} = 1;
+$keep_header_hash{'FattyAcid'} = 1;
+$keep_header_hash{'FA1'} = 1;
+$keep_header_hash{'FA2'} = 1;
+$keep_header_hash{'FA3'} = 1;
+$keep_header_hash{'FA4'} = 1;
+$keep_header_hash{'CalcMz'} = 1;
+$keep_header_hash{'IonFormula'} = 1;
+
+
 
 sub is_number
 {
@@ -107,20 +134,12 @@ sub reformat_sci
 }
 
 
-%guess_ion_charge_hash = ();    # crude guess at charge, purely based on +/-
 
-# known ion charges
-$known_ion_charge_hash{'+H'}     = 'pos';
-$known_ion_charge_hash{'+NH4'}   = 'pos';
-$known_ion_charge_hash{'+Na'}    = 'pos';
-$known_ion_charge_hash{'+H-H2O'} = 'pos';
-$known_ion_charge_hash{'-H'}     = 'neg';
-$known_ion_charge_hash{'+HCOO'}  = 'neg';
+%keep_col_hash = ();
+$num_files     = 0;
+$filename_pos  = 'lipidomics_split_pos.txt';
+$filename_neg  = 'lipidomics_split_neg.txt';
 
-
-$num_files = 0;
-$filename_pos = 'lipidomics_split_pos.txt';
-$filename_neg = 'lipidomics_split_neg.txt';
 for ($i = 0; $i < @ARGV; $i++)
 {
     $field = $ARGV[$i];
@@ -165,11 +184,30 @@ open OUTFILE_NEG, ">$filename_neg" or die "can't open output $filename_neg\n";
 
 # skip down to first line that has anything on it
 # lipidomics data has this issues sometimes
+%sample_rename_hash = ();
 while($line=<INFILE>)
 {
+    $line =~ s/[\r\n]+$//;
+
     # skip comment lines
+    # comment may contain sample renaming information, so check for it
     if ($line =~ /^#/)
     {
+        if ($line =~ /^#\[([^\]]+)\]:(.*?)\.raw\s*$/)
+        {
+            $sample_old = $1;
+
+            ## comment out for now, we'll rename later in the strip script
+            #$sample_new = $2;
+            #$sample_rename_hash{$sample_old} = $sample_new;
+            #printf STDERR "Renaming:\t%s\t%s\n", $sample_old, $sample_new;
+
+            # strip all the extra blank tabs/whitespace from end of line
+            $line =~ s/\s+$//;
+            
+            $rename_sample_line_hash{$sample_old} = $line;
+        }
+    
         next;
     }
 
@@ -209,6 +247,24 @@ if ($header_col_array[0] =~ /^id$/i)
 }
 
 
+# rename samples from comment lines in lipidomics data
+for ($col = 0; $col < @header_col_array; $col++)
+{
+    $field = $header_col_array[$col];
+
+    if ($field =~ /\[([^]]+)\]/)
+    {
+        $new_name = $sample_rename_hash{$1};
+        if (defined($new_name))
+        {
+            $field =~ s/\[[^]]+\]/\[$new_name\]/g;
+        }
+    }
+
+    $header_col_array[$col] = $field;
+}
+
+
 # conform sample columns
 for ($col = 0; $col < @header_col_array; $col++)
 {
@@ -243,24 +299,40 @@ for ($col = 0; $col < $num_header_cols; $col++)
     if ($field =~ / Peak height$/i)
     {
         $peak_height_flag = 1;
+        $keep_col_hash{$col} = 1;
+
         next;
     }
     if ($field =~ / Peak area$/i)
     {
         $peak_area_flag = 1;
+        $keep_col_hash{$col} = 1;
+
         next;
     }
     
     # lipidomics
-    if ($field =~ /^Area,/i)
+    if ($field =~ /^Area[,[]/i)
     {
-         $peak_area_flag = 1;
-         next;
+        $peak_area_flag = 1;
+        $keep_col_hash{$col} = 1;
+
+        next;
     }
-    if ($field =~ /^Height,/i)
+    if ($field =~ /^Height[,[]/i)
     {
-         $peak_height_flag = 1;
-         next;
+        $peak_height_flag = 1;
+        $keep_col_hash{$col} = 1;
+
+        next;
+    }
+    if ($field =~ /^Rt[,[]/i)
+    {
+        $keep_col_hash{$col} = 1;
+    }
+    if (defined($keep_header_hash{$field}))
+    {
+        $keep_col_hash{$col} = 1;
     }
 }
 
@@ -401,10 +473,11 @@ for ($col = 0; $col < $num_header_cols; $col++)
 
     if ($field =~ / Peak height$/i ||
         $field =~ / Peak area$/i ||
-        $field =~ /^Area,/i ||
-        $field =~ /^Height,/i)
+        $field =~ /^Area[,[]/i ||
+        $field =~ /^Height[,[]/i)
     {
         $sample_col_hash{$col} = 1;
+        $keep_col_hash{$col} = 1;
 
         if ($col < $first_abundance_col)
         {
@@ -415,7 +488,7 @@ for ($col = 0; $col < $num_header_cols; $col++)
     {
         if ($field =~ /\S/)
         {
-            $metadata_col_hash{$col} = 1;
+#            $metadata_col_hash{$col} = 1;
         }
     }
 }
@@ -442,8 +515,8 @@ if (!defined($name_col))
 #}
 
 
-@sample_col_array   = sort {$a <=> $b} keys %sample_col_hash;
-@metadata_col_array = sort {$a <=> $b} keys %metadata_col_hash;
+#@sample_col_array   = sort {$a <=> $b} keys %sample_col_hash;
+#@metadata_col_array = sort {$a <=> $b} keys %metadata_col_hash;
 
 
 # read in file, figure out which rows/columns are pos/neg later
@@ -505,40 +578,22 @@ for ($row = 0; $row < $num_rows; $row++)
 
     $pos_neg = '';
     $ion_str = '';
-    
+
     if ($lipid_ion =~ /(\+[A-Za-z0-9-]+)$/)
     {
-        $pos_neg = 'pos';
         $ion_str = $1;
-
-        $pos_neg_row_hash{$row} = $pos_neg;
     }
     elsif ($lipid_ion =~ /(\-[A-Za-z0-9-]+)$/)
     {
-        $pos_neg = 'neg';
         $ion_str = $1;
-
-        $pos_neg_row_hash{$row} = $pos_neg;
     }
-    
-    # overwrite with known pos/neg when possible
-    if ($ion_str ne '')
+
+
+    # look up known ion charges
+    if (defined($known_ion_charge_hash{$ion_str}))
     {
-        if (defined($known_ion_charge_hash{$ion_str}))
-        {
-            $pos_neg = $known_ion_charge_hash{$ion_str};
-            $pos_neg_row_hash{$row} = $pos_neg;
-        }
-        else
-        {
-            if (!defined($guess_ion_charge_hash{$ion_str}))
-            {
-                $guess_ion_charge_hash{$ion_str} = $pos_neg;
-                
-                printf STDERR "WARNING -- guessing unknown ion charge:\t%s\t%s\n",
-                    $ion_str, $pos_neg;
-            }
-        }
+        $pos_neg = $known_ion_charge_hash{$ion_str};
+        $pos_neg_row_hash{$row} = $pos_neg;
     }
     
     # count pos/neg for each sample
@@ -554,6 +609,10 @@ for ($row = 0; $row < $num_rows; $row++)
                 $col_counts_hash{$col}{$pos_neg} += 1;
             }
         }
+    }
+    else
+    {
+        $unknown_row_charge_hash{$row} = $ion_str;
     }
 }
 
@@ -575,145 +634,194 @@ foreach $col (@sample_col_array)
     }
 }
 
-@pos_col_array = sort {$a <=> $b} keys %pos_col_hash;
-@neg_col_array = sort {$a <=> $b} keys %neg_col_hash;
 
-$num_pos_cols = @pos_col_array;
-$num_neg_cols = @neg_col_array;
-
-$merge_flag = 0;
-if ($num_pos_cols && $num_neg_cols)
+# count pos/neg samples for all unknown adduct charges
+@unknown_row_charge_array = sort {$a<=>$b} keys %unknown_row_charge_hash;
+%unknown_ion_hash = ();
+foreach $row (@unknown_row_charge_array)
 {
-    printf STDERR "WARNING -- unmerged POS/NEG columns\n";
+    $ion_str = $unknown_row_charge_hash{$row};
     
-    if ($num_pos_cols != $num_neg_cols)
+    if (!defined($unknown_ion_hash{$ion_str}))
     {
-        printf STDERR "#POS/#NEG columns differ, leave unmerged:\t%d/%d\n",
-            $num_pos_cols, $num_neg_cols;
+        $unknown_ion_hash{$ion_str}{pos} = 0;
+        $unknown_ion_hash{$ion_str}{neg} = 0;
     }
-    else
-    {
-        $merge_flag = 1;
 
-        printf STDERR "WARNING -- assuming same input column order for pos/neg\n";
-        
-        if ($peak_height_flag)
-        {
-            for ($i = 0; $i < @pos_col_array; $i++)
-            {
-                $col_pos = $pos_col_array[$i];
-                $col_neg = $neg_col_array[$i];
+    foreach $col (@sample_col_array)
+    {
+        $field = $row_data_array[$row][$col];
             
-                $header_pos = $header_col_array[$col_pos];
-                $header_neg = $header_col_array[$col_neg];
-                
-                $name_pos   = '';
-                $metric_pos = '';
-                $name_neg   = '';
-                $metric_neg = '';
-                
-                if ($header_pos =~ /(.*?)\s*(Peak height|Peak area)$/)
-                {
-                    $name_pos   = $1;
-                    $metric_pos = $2;
-                }
-                if ($header_neg =~ /(.*?)\s*(Peak height|Peak area)$/)
-                {
-                    $name_neg   = $1;
-                    $metric_neg = $2;
-                }
-                
-                # remove any pos/neg that may already be in the sample names
-                # assume existing pos/neg are correctly labeled as such
-                #
-                # ^pos _pos_ _pos$
-                # ^neg _neg_ _neg$
-                #
-                if (!($name_pos =~ s/(^|[^A-Za-z0-9]+)pos([^A-Za-z0-9]+|$)/$2/i))
-                {
-                    $name_pos =~ s/pos$//i;
-                }
-                if (!($name_neg =~ s/(^|[^A-Za-z0-9]+)neg([^A-Za-z0-9]+|$)/$2/i))
-                {
-                    $name_neg =~ s/neg$//i;
-                }
-                
-                if ($metric_pos ne '' &&
-                    lc($metric_pos) eq lc($metric_neg))
-                {
-                    # in CICPT #3501, NEG has the smaller -# postfix,
-                    # so use NEG first, then POS
-                    #
-                    # 'neg' also comes before 'pos' alphabetically
-                    #
-                    $name_merged = $name_neg;
-                    if ($name_neg ne $name_pos)
-                    {
-                        $name_merged = sprintf "%s__%s", $name_neg, $name_pos;
-                    }
-                    
-                    # store col pair assignments for merging later
-                    $paired_pos_neg_hash{$col_pos}{name}   = $name_merged;
-                    $paired_pos_neg_hash{$col_pos}{metric} = $metric_neg;
-                    $paired_pos_neg_hash{$col_pos}{m_col}  = $col_neg;
-                    $paired_pos_neg_hash{$col_neg}{name}   = $name_merged;
-                    $paired_pos_neg_hash{$col_neg}{metric} = $metric_neg;
-                    $paired_pos_neg_hash{$col_neg}{m_col}  = $col_pos;
-                    
-                    printf STDERR "PAIRING:\t%d\t%d\t%s\t%s\n",
-                        $col_neg, $col_pos,
-                        $metric_neg,
-                        $name_merged;
-                }
+        if (is_number($field) && $field != 0)
+        {
+            if (defined($pos_col_hash{$col}))
+            {
+                $unknown_ion_hash{$ion_str}{pos} += 1;
+            }
+            elsif (defined($neg_col_hash{$col}))
+            {
+                $unknown_ion_hash{$ion_str}{neg} += 1;
             }
         }
     }
 }
 
+# infer unknown adduct charges from pos/neg sample patterns
+foreach $ion_str (sort keys %unknown_ion_hash)
+{
+    $count_pos = $unknown_ion_hash{$ion_str}{pos};
+    $count_neg = $unknown_ion_hash{$ion_str}{neg};
+    
+    if ($count_pos && $count_neg == 0)
+    {
+        $known_ion_charge_hash{$ion_str} = 'pos';
+
+        printf STDERR "Inferring unknown adduct charge:\t%s\t%s\n",
+            $ion_str, 'pos';
+    }
+    elsif ($count_neg && $count_pos == 0)
+    {
+        $known_ion_charge_hash{$ion_str} = 'neg';
+
+        printf STDERR "Inferring unknown adduct charge:\t%s\t%s\n",
+            $ion_str, 'neg';
+    }
+    else
+    {
+        printf STDERR "WARNING -- skipping misbehaving unknown adduct:\t%s\n",
+            $ion_str;
+    }
+}
+
+# assign the newly inferred charges to remaining rows
+foreach $row (@unknown_row_charge_array)
+{
+    $ion_str = $unknown_row_charge_hash{$row};
+    
+    $pos_neg = $known_ion_charge_hash{$ion_str};
+    
+    if (defined($pos_neg))
+    {
+        $pos_neg_row_hash{$row} = $pos_neg;
+    }
+}
+
+
+@pos_col_array = sort {$a <=> $b} keys %pos_col_hash;
+@neg_col_array = sort {$a <=> $b} keys %neg_col_hash;
+
+#$num_pos_cols = @pos_col_array;
+#$num_neg_cols = @neg_col_array;
+
+
+# scan through positive and negative columns
+# extract out lipidomics sample names so that we can pull in matching Rt
+foreach $col (@pos_col_array)
+{
+    $header = $header_col_array[$col];
+    
+    if ($header =~ /^[^[]+[,[]([^[]+)\]/)
+    {
+        $sample = $1;
+        
+        $pos_sample_hash{$sample} = 1;
+    }
+}
+foreach $col (@neg_col_array)
+{
+    $header = $header_col_array[$col];
+    
+    if ($header =~ /^[^[]+[,[]([^[]+)\]/)
+    {
+        $sample = $1;
+        
+        $neg_sample_hash{$sample} = 1;
+    }
+}
+for ($col = 0; $col < @header_col_array; $col++)
+{
+    if (!defined($keep_col_hash{$col})) { next; }
+
+    $header = $header_col_array[$col];
+
+    if ($header =~ /^[^[]+[,[]([^[]+)\]/)
+    {
+        $sample = $1;
+        
+        if (defined($pos_sample_hash{$sample}))
+        {
+            $pos_col_hash{$col} = 1;
+        }
+        if (defined($neg_sample_hash{$sample}))
+        {
+            $neg_col_hash{$col} = 1;
+        }
+    }
+}
+
+
+# print sample rename comment lines if we haven't renamed anything
+@temp_array = sort keys %sample_rename_hash;
+if (@temp_array == 0)
+{
+    $temp_pos_flag = 0;
+    $temp_neg_flag = 0;
+
+    foreach $sample (sort keys %pos_sample_hash)
+    {
+        $line = $rename_sample_line_hash{$sample};
+        if (defined($line))
+        {
+            print OUTFILE_POS "$line\n";
+            
+            $temp_pos_flag = 1;
+        }
+    }
+    foreach $sample (sort keys %neg_sample_hash)
+    {
+        $line = $rename_sample_line_hash{$sample};
+        if (defined($line))
+        {
+            print OUTFILE_NEG "$line\n";
+
+            $temp_neg_flag = 1;
+        }
+    }
+    
+    # print blank lines to separate comments from true header
+    if ($temp_pos_flag)
+    {
+        print OUTFILE_POS "\n";
+    }
+    if ($temp_neg_flag)
+    {
+        print OUTFILE_NEG "\n";
+    }
+}
 
 
 # print new header lines
-%skip_paired_col_hash = ();
 $header_pos = '';
 $header_neg = '';
-foreach $col (@metadata_col_array)
+for ($col = 0; $col < @header_col_array; $col++)
 {
-    $header = $header_col_array[$col];
-    
-    if ($header_pos ne '') { $header_pos .= "\t"; }
-    if ($header_neg ne '') { $header_neg .= "\t"; }
-    
-    $header_pos .= $header;
-    $header_neg .= $header;
-}
-foreach $col (@sample_col_array)
-{
+    if (!defined($keep_col_hash{$col})) { next; }
+
     $header = $header_col_array[$col];
 
-    # skip paired columns we've already dealt with
-    if (defined($skip_paired_col_hash{$col}))
+    if (defined($keep_header_hash{$header}) ||
+        defined($pos_col_hash{$col}))
     {
-        next;
+        if ($header_pos ne '') { $header_pos .= "\t"; }
+        $header_pos .= $header;
     }
 
-    # merge paired columns into a single header
-    if (defined($paired_pos_neg_hash{$col}))
+    if (defined($keep_header_hash{$header}) ||
+        defined($neg_col_hash{$col}))
     {
-        $name_merged = $paired_pos_neg_hash{$col}{name};
-        $metric      = $paired_pos_neg_hash{$col}{metric};
-
-        $header = $name_merged . ' ' . $metric;
-
-        # skip matched column later, since we're dealing with it here
-        $col_match = $paired_pos_neg_hash{$col}{m_col};
-        $skip_paired_col_hash{$col_match} = 1;
+        if ($header_neg ne '') { $header_neg .= "\t"; }
+        $header_neg .= $header;
     }
-    
-    if ($header_pos ne '') { $header_pos .= "\t"; }
-    if ($header_neg ne '') { $header_neg .= "\t"; }
-    
-    $header_pos .= 'pos_' . $header;
-    $header_neg .= 'neg_' . $header;
 }
 print OUTFILE_POS "$header_pos\n";
 print OUTFILE_NEG "$header_neg\n";
@@ -727,47 +835,28 @@ for ($row = 0; $row < $num_rows; $row++)
 {
     $line_pos = '';
     $line_neg = '';
-    foreach $col (@metadata_col_array)
+    for ($col = 0; $col < @header_col_array; $col++)
     {
-        $field = $row_data_array[$row][$col];
-    
-        if ($line_pos ne '') { $line_pos .= "\t"; }
-        if ($line_neg ne '') { $line_neg .= "\t"; }
-    
-        $line_pos .= $field;
-        $line_neg .= $field;
-    }
-    foreach $col (@sample_col_array)
-    {
-        $field = $row_data_array[$row][$col];
+        if (!defined($keep_col_hash{$col})) { next; }
 
-        # skip paired columns we've already dealt with
-        if (defined($skip_paired_col_hash{$col}))
+        $header = $header_col_array[$col];
+        $field  = $row_data_array[$row][$col];
+    
+        if (defined($keep_header_hash{$header}) ||
+            defined($pos_col_hash{$col}))
         {
-            next;
+            if ($line_pos ne '') { $line_pos .= "\t"; }
+            $line_pos .= $field;
         }
 
-        # merge paired columns into a single field
-        if (defined($paired_pos_neg_hash{$col}))
+        if (defined($keep_header_hash{$header}) ||
+            defined($neg_col_hash{$col}))
         {
-            $col_match = $paired_pos_neg_hash{$col}{m_col};
-            
-            # if current column is missing/zero, use the matched col instead
-            if ($field == 0)
-            {
-                $field = $row_data_array[$row][$col_match];
-            }
-
-            # skip matched column later, since we're dealing with it here
-            $skip_paired_col_hash{$col_match} = 1;
+            if ($line_neg ne '') { $line_neg .= "\t"; }
+            $line_neg .= $field;
         }
-    
-        if ($line_pos ne '') { $line_pos .= "\t"; }
-        if ($line_neg ne '') { $line_neg .= "\t"; }
-    
-        $line_pos .= $field;
-        $line_neg .= $field;
     }
+
     
     # output positive ions to positive file
     # output negative ions to negative file
@@ -788,9 +877,9 @@ for ($row = 0; $row < $num_rows; $row++)
     }
     else
     {
-        $lipid_ion = $row_data_array[$row][$name_col];
+        $ion_str = $row_data_array[$row][$name_col];
 
-        printf STDERR "Skipping unknown ion charge:\t%s\n", $lipid_ion;
+        printf STDERR "WARNING -- skipping unknown adduct row:\t%s\n", $ion_str;
     }
 }
 
