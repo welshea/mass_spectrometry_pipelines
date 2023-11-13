729a35815e15a41f94f8d73e1e5e05aca1ea23e2
Eric A. Welsh <eric.welsh@moffitt.org>
2023-11-13 13:12:26 -0500

    support TMT-2;
    MOD:  autodetect_ms_data.pl;
          [Mon Nov 13 13:12:26 2023 -0500]

--- autodetect_ms_data.pl
+++ autodetect_ms_data.pl
@@ -2,6 +2,7 @@
 
 # Changelog:
 #
+# 2023-11-13: support TMT-2
 # 2023-11-13: support iTRAQ-4 and iTRAQ-8
 # 2023-08-23: add Ensembl accessions detection
 # 2023-08-23: bugfix proteogeonomics 2-character chromosomes
@@ -37,14 +38,17 @@ $channel_map_table[4][1]   = '115';
 $channel_map_table[4][2]   = '116';
 $channel_map_table[4][3]   = '117';
 
-$channel_map_table[8][0]  = '113';	# iTRAQ
-$channel_map_table[8][1]  = '114';
-$channel_map_table[8][2]  = '115';
-$channel_map_table[8][3]  = '116';
-$channel_map_table[8][4]  = '117';
-$channel_map_table[8][5]  = '118';
-$channel_map_table[8][6]  = '119';
-$channel_map_table[8][7]  = '121';
+$channel_map_table[8][0]   = '113';	# iTRAQ
+$channel_map_table[8][1]   = '114';
+$channel_map_table[8][2]   = '115';
+$channel_map_table[8][3]   = '116';
+$channel_map_table[8][4]   = '117';
+$channel_map_table[8][5]   = '118';
+$channel_map_table[8][6]   = '119';
+$channel_map_table[8][7]   = '121';
+
+$channel_map_table[2][0]   = '126C';	# 126C
+$channel_map_table[2][1]   = '127C';
 
 $channel_map_table[6][0]   = '126C';	# 126C
 $channel_map_table[6][1]   = '127C';
